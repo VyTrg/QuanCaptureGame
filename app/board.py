c9f172739dfffead5648083222b8fcbfe80a7478
VyTrg <140966315+VyTrg@users.noreply.github.com>
2025-04-24 13:18:24 +0700

    update for ignore

--- app/board.py
+++ app/board.py
@@ -1,5 +1,37 @@
-import pygame
+import pygame as pg
 
 class Board:
-    def __init__(self):
-        pass
+    def __init__(self, screen):
+        self.screen = screen
+        self.tile_size = 120
+        self.rows = 2
+        self.cols = 5
+
+        
+        self.square = pg.image.load('../image/default.png')
+        self.quan_trai = pg.image.load('../image/quanTrai.png')
+        self.quan_phai = pg.image.load('../image/quanPhai.png')
+
+        
+        self.square = pg.transform.scale(self.square, (self.tile_size, self.tile_size))
+        self.quan_trai = pg.transform.scale(self.quan_trai, (self.tile_size, self.tile_size * 2))
+        self.quan_phai = pg.transform.scale(self.quan_phai, (self.tile_size, self.tile_size * 2))
+
+    def draw(self):
+       
+       
+        for col in range(self.cols):
+            x = col * self.tile_size + 200
+            y = 100
+            self.screen.blit(self.square, (x, y))
+            
+
+        
+        for col in range(self.cols):
+            x = (self.cols - 1 - col) * self.tile_size + 200
+            y = 220
+            self.screen.blit(self.square, (x, y))
+            
+        #mandarin draw for both sides
+        self.screen.blit(self.quan_trai, (80, 100))
+        self.screen.blit(self.quan_phai, (800, 100))
--- app/main.py
+++ app/main.py
@@ -1,10 +1,11 @@
 import pygame as pg
-
+from board import Board
 pg.init()
 clock = pg.time.Clock()
 screen = pg.display.set_mode((1000, 600))
 bg_image = pg.image.load('../image/bg.jpg')
-
+bg_image = pg.transform.scale(bg_image, screen.get_size())  
+board = Board(screen)
 running = True
 while running:
     for event in pg.event.get():
@@ -12,6 +13,7 @@ while running:
             running = False
 
     screen.blit(bg_image, (0, 0))  
-    pg.display.flip()              
+    board.draw()              
+    pg.display.flip()
     clock.tick(60)                 
 pg.quit()
