3e483066fc3e4dddc1fbba259fc1e923c8fab02d
hhtran2002 <huatrancm123@gmail.com>
2025-04-25 11:26:45 +0700

    update 25/4

--- app/main.py
+++ app/main.py
@@ -1,11 +1,14 @@
 import pygame as pg
 from board import Board
+from scoreBoard import ScoreBoard
+
 pg.init()
 clock = pg.time.Clock()
 screen = pg.display.set_mode((1000, 600))
 bg_image = pg.image.load('../image/bg.jpg')
 bg_image = pg.transform.scale(bg_image, screen.get_size())  
 board = Board(screen)
+scoreboard = ScoreBoard()
 running = True
 while running:
     for event in pg.event.get():
@@ -13,7 +16,9 @@ while running:
             running = False
 
     screen.blit(bg_image, (0, 0))  
-    board.draw()              
+    board.draw()  
+    scoreboard.draw(screen)
+            
     pg.display.flip()
     clock.tick(60)                 
 pg.quit()
